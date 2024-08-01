771e6dd0ba4a11d5b6ee9ebf0a02843d088ff11e
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2024-08-01 15:04:53 -0300

    Fix template

--- src/middlewares/handlePrismaError.ts
+++ src/middlewares/handlePrismaError.ts
@@ -1,4 +1,4 @@
-import { Prisma } from '@prisma/client';
+import { PrismaClientKnownRequestError, PrismaClientValidationError } from '@prisma/client/runtime/react-native.js';
 import { ErrorRequestHandler } from 'express';
 import HttpError from 'http-errors';
 
@@ -6,8 +6,8 @@ import HttpError from 'http-errors';
 // * as a BadRequest
 const handlePrismaError: ErrorRequestHandler = (error, req, res, next) => {
   if (
-    error instanceof Prisma.PrismaClientKnownRequestError ||
-    error instanceof Prisma.PrismaClientValidationError
+    error instanceof PrismaClientKnownRequestError ||
+    error instanceof PrismaClientValidationError
   ) {
     return next(new HttpError.BadRequest(error.message));
   }
